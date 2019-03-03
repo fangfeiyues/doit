812e2c3c80c655130fa2e0ee665a779f45c3c1c2
fangfeiyue <fangfeiyues@163.com>
2019-03-03 16:58:12 +0800

    commit first time

---
+++ src/main/java/com/fang/Hello.java
@@ -0,0 +1,11 @@
+package com.fang;
+
+/**
+ * created by fang on 2019/3/3/003 16:53
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hollo java");
+    }
+}
