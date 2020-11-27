c40d501cb7bef8298f9bf1af625de0e7eb14dc04
fangfeiyue <fangfeiyue@youzan.com>
2020-11-27 22:01:20 +0800

    无重叠区间

--- src/main/java/com/fang/doit/algo/lc/CanCompleteCircuit.java
+++ src/main/java/com/fang/doit/algo/lc/CanCompleteCircuit.java
@@ -1,5 +1,8 @@
 package com.fang.doit.algo.lc;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  * @author created by fang on 2020/11/18/018 1:15
  */
@@ -14,8 +17,36 @@ public class CanCompleteCircuit {
      */
     public static int canCompleteCircuit(int[] gas, int[] cost) {
         //
+        return canCompleteCircuitByViolence(gas, cost);
+    }
 
-        return -1;
+    /**
+     * 435 无重叠区间
+     * 执行耗时:7 ms,击败了12.18% 的Java用户 内存消耗:39 MB,击败了20.05% 的Java用户
+     *
+     * @param intervals
+     * @return
+     */
+    public int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals.length == 0) {
+            return 0;
+        }
+        // 按照数组的最后一位排序 尾越小留给后面的空间越大
+        Arrays.sort(intervals, Comparator.comparing(o -> o[1]));
+        // 即第0个数组第1位
+        int end = intervals[0][1];
+        int counter = 1;
+        for (int i = 1; i < intervals.length; i++) {
+            // 后一个的头小于前一个的尾巴 需要移除
+            if (intervals[i][0] < end) {
+                continue;
+            }
+            end = intervals[i][1];
+            counter++;
+        }
+
+        // 解答失败: 测试用例:[[1,2],[1,2]] 测试结果:1 期望结果:0 stdout:
+        return intervals.length - counter;
     }
 
 
